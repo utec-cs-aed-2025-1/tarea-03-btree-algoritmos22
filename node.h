368655e373a6d9db0d019ce9719b90c3e6848b08
EliseoDavid <eliseodv10@gmail.com>
2025-11-03 11:11:12 -0500

    update

--- node.h
+++ node.h
@@ -5,26 +5,23 @@ using namespace std;
 
 template <typename TK>
 struct Node {
-  // array de keys
   TK* keys;
-  // array de punteros a hijos
   Node** children;
-  // cantidad de keys
   int count;
-  // indicador de nodo hoja
   bool leaf;
 
-  Node() : keys(nullptr), children(nullptr), count(0) {}
+  Node() : keys(nullptr), children(nullptr), count(0), leaf(true) {}
   Node(int M) {
     keys = new TK[M - 1];
     children = new Node<TK>*[M];
+    for (int i = 0; i < M; i++) {
+        children[i] = nullptr;
+    }
     count = 0;
     leaf = true;
   }
 
-  void killSelf() {
-    // TODO
-  }
+  void killSelf();
 };
 
 #endif
